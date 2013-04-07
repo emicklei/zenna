05042a0faff41836432bac0897dcf74197499554
Ernest Micklei <ernest.micklei@gmail.com>
2013-04-07 22:31:22 +0200

    tripletooth using pen

--- trapezia/tripletooth.go
+++ trapezia/tripletooth.go
@@ -10,12 +10,46 @@ import (
 
 func main() {
 	canvas := svg.New(os.Stdout)
-	canvas.Start(600, 600)
-	canvas.Title("Trapezium")
-	canvas.Translate(300, 300)
+	canvas.Start(300, 300)
+	canvas.Title("TripleTooth")
+	canvas.Translate(150, 150)
 	canvas.Scale(100)
 
 	painter := NewSVGPainter("", canvas)
+
+	usePen(painter)
+	// useTrapezia(painter)
+
+	canvas.Gend() //scale
+	canvas.Gend() //translat
+	canvas.End()  // canvas
+}
+
+func usePen(painter Painter) {
+	painter.Style("stroke-width:0.01px;stroke:blue;fill:none")
+	p := NewPen(PointZero, P(1, 1))
+	p.Move(0, 0.5)
+	p.Move(-0.5*Cos30, -0.25)
+	p.Move(0, 0.5)
+	p.Move(0.5*Cos30, 0.25)
+	p.Move(0.5*Cos30, -0.25)
+	p.Move(0, -1)
+	p.Move(0.5*Cos30, -0.25)
+	p.Move(0, 0.5)
+	p.Move(0.5*Cos30, -0.25)
+	p.Move(0, -0.5)
+	p.Move(-0.5*Cos30, -0.25)
+	p.Move(-Cos30, 0.5)
+	p.Move(-0.5*Cos30, -0.25)
+	p.Move(0.5*Cos30, -0.25)
+	p.Move(-0.5*Cos30, -0.25)
+	p.Move(-0.5*Cos30, 0.25)
+	p.Move(0, 0.5)
+	p.Move(Cos30, 0.5)
+	painter.Paint(Polygon{p.Path()})
+}
+
+func useTrapezia(painter Painter) {
 	t1 := Trapezium{60, 1}
 	dx := t1.Delta().X
 	dy := t1.Delta().Y
@@ -24,17 +58,18 @@ func main() {
 	// x-y
 	//painter.Paint(StyleWith{Raster{PointZero, 6, 6, 0, 0}, "stroke-width:0.01px;stroke:red"})
 
-	painter.Style("stroke-width:0.01px;stroke:black;fill:none")
+	painter.Style("stroke-width:0.01px;stroke:blue;fill:blue")
 
 	g0 := new(Composite)
 	g0.Add(TranslateBy{RotateBy{t1, 90}, P(2*dy/3, 0)})
 	g0.Add(TranslateBy{RotateBy{t1, 30}, P(-2*dx-(dy/3), 0.5*t1.Length)})
-	painter.Paint(g0)
 
-	painter.Paint(RotateBy{g0, 120})
-	painter.Paint(RotateBy{g0, 240})
+	g1 := new(Composite)
+	g1.Add(g0)
+	g1.Add(RotateBy{g0, 120})
+	g1.Add(RotateBy{g0, 240})
 
-	canvas.Gend() //scale
-	canvas.Gend() //translat
-	canvas.End()  // canvas
+	painter.Paint(g1)
+	painter.Paint(TranslateBy{RotateBy{g1, -60}, P(-t1.Length*Cos30+(dy/3), -2)})
+
 }
