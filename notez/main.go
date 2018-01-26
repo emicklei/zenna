c22fac464468a137fdec46d99d560aecf75f11a9
Ernest Micklei <>
2018-01-26 23:48:55 +0100

    2 note keys

--- notez/main.go
+++ notez/main.go
@@ -13,11 +13,18 @@ const (
 	stickStyle   = "stroke-width:2px;stroke:black;stroke-linecap:round"
 	bubbleStyle  = "stroke-width:2px;stroke:black;fill:black"
 	barStyle     = "stroke-width:2px;stroke:black"
-	barWidth     = 800
-	auxBarStyle  = "stroke-width:3px;stroke:black"
-	stickSpaceX  = 36.0
+	barWidth     = 1200
+	auxBarStyle  = "stroke-width:2px;stroke:black"
+	noteStyle    = "font-weight:bold"
+	stickSpaceX  = 42.0
 	bubbleSpaceY = 17.0
 	c            = 0
+	downTextY    = -50
+	downTextDY   = 16
+	downTextDX   = -18
+	upTextY      = 160
+	upTextDY     = 100
+	upTextDX     = 8
 )
 
 var upBar = Style{auxBarStyle,
@@ -29,13 +36,20 @@ func main() {
 	canvas := svg.New(os.Stdout)
 	canvas.Start(2400, 1200)
 	canvas.Title("Musik Notez")
-	canvas.Translate(barWidth/2, 200)
-
 	painter := svgf.NewSVGPainter("", canvas)
 
-	painter.Paint(Style{"stroke-width:1px;stroke:red", LineSegment{P(-200, 0), P(200, 0)}})
-	painter.Paint(Style{"stroke-width:1px;stroke:red", LineSegment{P(0, 200), P(0, -200)}})
+	canvas.Translate(barWidth/2, 240)
+	key(painter, GnoteName)
+	canvas.Gend() // Translate
 
+	canvas.Translate(barWidth/2, 480)
+	key(painter, FnoteName)
+	canvas.Gend() // Translate
+
+	canvas.End()
+}
+
+func key(painter svgf.Painter, nameFunc func(int) string) {
 	stick := Style{stickStyle,
 		LineSegment{P(0, 0), P(0, 60)}}
 	bubble := Translate{
@@ -56,29 +70,32 @@ func main() {
 
 	bars(painter)
 
-	for t := 1; t < 10; t++ {
-		note := c + t - 1
+	for t := 1; t < 14; t++ {
+		note := c + t
 		off := P(float64(t)*stickSpaceX-(stickSpaceX/2), float64(t)*bubbleSpaceY*0.5)
 		auxbars(painter, off, note)
 		painter.Paint(Translate{off, upNote})
-		painter.Paint(Translate{off, Text{Text: strconv.Itoa(note)}})
+		painter.Paint(Translate{P(off.X+upTextDX, off.Y+upTextDY), Style{noteStyle, Text{Text: nameFunc(note)}}})
 	}
-	for t := 0; t < 10; t++ {
+	for t := 0; t < 14; t++ {
 		note := c - t
 		off := P(float64(t)*-stickSpaceX, float64(t)*bubbleSpaceY*-0.5)
 		auxbars(painter, off, note)
 		painter.Paint(Translate{off, downNote})
-		painter.Paint(Translate{off, Text{Text: strconv.Itoa(note)}})
+		painter.Paint(Translate{P(off.X+downTextDX, off.Y+downTextDY), Style{noteStyle, Text{Text: nameFunc(note)}}})
 	}
-
-	canvas.Gend() // Translate
-	canvas.End()
 }
 
 func auxbars(painter svgf.Painter, offset Point, note int) {
-	for t := 0; t < 10; t++ {
-		delta := P(0, float64(t)*bubbleSpaceY*0.5)
-		painter.Paint(Translate{offset.Plus(delta), upBar})
+	if note < -4 {
+		for t := -2; t > note+2; t -= 2 {
+			painter.Paint(Translate{P(offset.X, float64(t-3)*bubbleSpaceY*0.5), downBar})
+		}
+	}
+	if note > 6 {
+		for t := 4; t < note-2; t += 2 {
+			painter.Paint(Translate{P(offset.X, float64(t+3)*bubbleSpaceY*0.5), upBar})
+		}
 	}
 }
 
@@ -88,4 +105,52 @@ func bars(painter svgf.Painter) {
 			LineSegment{P(-barWidth/2, float64(h)*bubbleSpaceY+34.5), P(barWidth/2, float64(h)*bubbleSpaceY+34.5)}}
 		painter.Paint(bar)
 	}
+}
+
+func GnoteName(note int) string {
+	n := note % 7
+	if n < 0 {
+		n = 7 - (-note % 7)
+	}
+	switch n {
+	case 0:
+		return "A"
+	case 1:
+		return "B"
+	case 2:
+		return "C"
+	case 3:
+		return "D"
+	case 4:
+		return "E"
+	case 5:
+		return "F"
+	case 6:
+		return "G"
+	}
+	return strconv.Itoa(note)
+}
+
+func FnoteName(note int) string {
+	n := note % 7
+	if n < 0 {
+		n = 7 - (-note % 7)
+	}
+	switch n {
+	case 0:
+		return "C"
+	case 1:
+		return "D"
+	case 2:
+		return "E"
+	case 3:
+		return "F"
+	case 4:
+		return "G"
+	case 5:
+		return "A"
+	case 6:
+		return "B"
+	}
+	return strconv.Itoa(note)
 }
